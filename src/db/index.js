ef93032e33d779dc02f5a98cc515c606d8ae8e73
tetiana <tetianayaremko@gmail.com>
2020-10-14 06:40:25 +0200

    update

--- src/db/index.js
+++ src/db/index.js
@@ -14,49 +14,56 @@ const sequelize = new Sequelize(
     },
   }
 );
-const models = {
-  User: require("../services/users/UserSchema")(sequelize, Sequelize),
-  Timetable: require("../services/timetable/schemas/TimeTableSchema")(
-    sequelize,
-    Sequelize
-  ),
-  Class: require("../services/timetable/schemas/ClassSchema")(
-    sequelize,
-    Sequelize
-  ),
-  TimetableFreeSpace: require("../services/timetable/schemas/TimetableFreeSpace")(
-    sequelize,
-    Sequelize
-  ),
-  ClassFilledSpace: require("../services/timetable/schemas/ClassFilledSpace")(
-    sequelize,
-    Sequelize
-  ),
-  Group: require("../services/timetable/schemas/GroupSchema")(
-    sequelize,
-    Sequelize
-  ),
-  Classroom: require("../services/timetable/schemas/ClassRoomSchema")(
-    sequelize,
-    Sequelize
-  ),
-  ClassroomClass: require("../services/timetable/schemas/ClassromClass")(
-    sequelize,
-    Sequelize
-  ),
-  GroupClass: require("../services/timetable/schemas/GroupClass")(
-    sequelize,
-    Sequelize
-  ),
-};
+const db = {};
 
-Object.keys(models).forEach((modelName) => {
-  if ("associate" in models[modelName]) {
-    models[modelName].associate(models);
-  }
-});
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
 
-models.sequelize = sequelize;
-models.Sequelize = Sequelize;
+//Models/tables
+db.customers = require("../services/users/UserSchema")(sequelize, Sequelize);
+// const models = {
+//   User: require("../services/users/UserSchema")(sequelize, Sequelize),
+//   Timetable: require("../services/timetable/schemas/TimeTableSchema")(
+//     sequelize,
+//     Sequelize
+//   ),
+//   Class: require("../services/timetable/schemas/ClassSchema")(
+//     sequelize,
+//     Sequelize
+//   ),
+//   TimetableFreeSpace: require("../services/timetable/schemas/TimetableFreeSpace")(
+//     sequelize,
+//     Sequelize
+//   ),
+//   ClassFilledSpace: require("../services/timetable/schemas/ClassFilledSpace")(
+//     sequelize,
+//     Sequelize
+//   ),
+//   Group: require("../services/timetable/schemas/GroupSchema")(
+//     sequelize,
+//     Sequelize
+//   ),
+//   Classroom: require("../services/timetable/schemas/ClassRoomSchema")(
+//     sequelize,
+//     Sequelize
+//   ),
+//   ClassroomClass: require("../services/timetable/schemas/ClassromClass")(
+//     sequelize,
+//     Sequelize
+//   ),
+//   GroupClass: require("../services/timetable/schemas/GroupClass")(
+//     sequelize,
+//     Sequelize
+//   ),
+// };
+//
+// Object.keys(models).forEach((modelName) => {
+//   if ("associate" in models[modelName]) {
+//     models[modelName].associate(models);
+//   }
+// });
+//
+// models.sequelize = sequelize;
+// models.Sequelize = Sequelize;
 
-module.exports = models;
+module.exports = db;
